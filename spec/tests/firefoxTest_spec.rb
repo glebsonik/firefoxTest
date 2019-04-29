a02d3a31cc9dc38c6cf710a76344942a41ea2b29
glebsonik <leeroyglebchik@gmail.com>
2019-04-29 23:52:23 +0300

    Added new branch withBeforeBehaviour. Trying to use before hook method for right testing behaviour.

--- spec/tests/firefoxTest_spec.rb
+++ spec/tests/firefoxTest_spec.rb
@@ -2,12 +2,24 @@ require_relative '../spec_helper.rb'
 
 RSpec.describe 'firefox google registration and authorization', type: :feature do
 
-context 'When first time accessing the sing in page' do
-    it 'should have content Создайте аккаунт Google' do
-      visit 'https://accounts.google.com/signup'
-      expect(page).to have_content('Создайте аккаунт Google')
+  before(:each) do
+    visit "https://accounts.google.com/signup"
+  end
 
-      find("//input[@id='lastName']").set('testName')
+  context "When first time accessing the sing in page" do
+
+    it "should have content Создайте аккаунт Google" do
+      expect(page).to have_content("Создайте аккаунт Google")
+    end
+
+    it "should fill all blanks on the page and go further" do
+      find("//input[@id='lastName']").set("testLastName")
+      find("//input[@id='firstName']").set("testFirstName")
+      find("//input[@id='username']").set("testUser322228")
+      test_password = "123456G34"
+      find("//div[@id='passwd']//input").set(test_password)
+      find("//div[@id='confirm-passwd']//input").set(test_password)
+      find("//*[@id='accountDetailsNext']").click()
     end
   end
 end
